6ce140d1ec0422d1ef833912657053ee5911f518
Juliet Wang <Julietw2@uci.edu>
2026-01-12 00:37:49 +0000

    modified loop to print the pattern with indentation

--- .ipynb_checkpoints/a0-checkpoint.py
+++ .ipynb_checkpoints/a0-checkpoint.py
@@ -7,13 +7,22 @@
 # Juliet Wang
 # Julietw2@uci.edu
 # 65183805
+
+#main method
 def main():
     #user input
     n = int(input())
+    #draws rectangles
     def pattern():
-        for i in range(1,n):
-            print("+-+\n| |\n+-+")
+        #loops based on input of # of rectangles
+        for i in range(n):
+            #indents based on i 
+            ind = " " * i 
+            #prints shape
+            print(ind + "+-+")
+            print(ind + "| |")
+            print(ind + "+-+")
     pattern()
+
 if __name__ == "__main__":
     main()
-    
